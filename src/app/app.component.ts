9553360ffb63a29044feffcefc9d10e133a3a4b1
Rasheed <adil-rasheed@mercer.com>
2019-02-02 23:29:00 +0530

    add video app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  videoURL: string = '';
+  iframeURL: SafeUrl
+  trustedDashboardUrl: SafeUrl;
+  urlData = [];
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  public getIframeVideoUrl() {
+
+    this.setIFrameURL(this.videoURL);
+   
+  }
+
+  setIFrameURL(url: string) {
+    if (!url) {
+      return '';
+    }
+    this.urlData.push(url);  
+    let _url = url.replace('watch', 'embed')
+    this.iframeURL = this.sanitizer.bypassSecurityTrustResourceUrl(_url);   
+    if(this.urlData.length>3)
+    { 
+      this.urlData.shift();
+    }
+    this.clearInput();
+  }
+  clearInput()
+  {
+    this.videoURL =null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatButtonModule, MatListModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
